4032d0f9ef13f53a72227d8472cdc633757f79e4
zycai <caizhuoyue77@gmail.com>
2024-05-16 13:40:06 +0800

    add button for checking the 编排结果

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,28 @@
 from openai import OpenAI
 import streamlit as st
 
+if "button_clicked" not in st.session_state:
+    st.session_state["button_clicked"] = True
 
 def check_yes():
-    st.session_state.messages.append({"role": "assistant", "content": "点击按钮"})
+    # 应该把编排得到的API依次调用
+    # 还是得通过ReWOO来执行编排
+    st.session_state.button_clicked = True
 
 with st.sidebar:
     qwen_api_key = st.text_input("Qwen API Key", key="qwen_api_key", type="password")
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    mode = st.selectbox("Mode", ["ReWOO", "ReACT","LLMCompiler","Manual"])
-    check_yes = st.button("执行编排流程", on_click=check_yes, disabled=True, use_container_width=True)
-
+    
+    """
+    编排模式：
+    1.Auto：直接以ReWOO的形式执行
+    2.Validation：以ReWOO方式生成API编排计划，通过人工确认后再执行
+    3.Manual：人工编排整个API流程
+    """
+    mode = st.selectbox("Mode",["Auto","Semi-Auto","Manual"])
+    # mode = st.selectbox("Mode", ["ReWOO", "ReACT","LLMCompiler","Manual"])
+    if not st.session_state.button_clicked:
+        check_yes = st.button("执行编排流程", key="ok", on_click=check_yes, disabled=False, use_container_width=True)
 
 st.title("蔡卓悦的API编排demo")
 st.caption("🚀 通过ReWOO方式一次生成全部的API编排计划，然后依次执行")
@@ -22,8 +34,6 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    # 原本是和OpenAI的API交互，现在改成和本地的API交互
-    # client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     
@@ -39,6 +49,8 @@ if prompt := st.chat_input():
 
     if response.status_code == 200:
         msg = response.json()["response"]
+        st.session_state["button_clicked"] = False
+        st.experimental_rerun()
     else:
         msg = "API调用失败"
     
